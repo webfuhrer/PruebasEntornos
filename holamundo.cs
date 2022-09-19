61e22b7a9248ef8c0c7ae74efa87bd418660a7e3
Luis Sanz Goizueta <ataraxa@hotmail.com>
2022-09-19 12:22:50 +0200

    Create holamundo.cs

---
+++ holamundo.cs
@@ -0,0 +1,12 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello Mono World");
+    }
+}
